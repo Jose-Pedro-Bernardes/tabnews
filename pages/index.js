f37b4b5d0286a01c310fe86f59720baebc825e6f
Jose-Pedro-Bernardes <josepedro.bernardes1@gmail.com>
2026-09-17 15:42:17 -0300

    feat: Adding Home and finish the first challenge

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+function Home () {
+  return (
+    <div>
+      <h1>Vovó</h1>
+      <p>Eu queria muito ter te conhecido,
+         sinto que nesses momentos o que eu
+          mais queria era conversar com a senhora,
+           distrair, apenas passar o tempo escutando
+            suas histórias. Eu te amo mesmo sem nunca
+             ter te conhecido...</p>
+    </div>
+  )
+}
+
+export default Home
